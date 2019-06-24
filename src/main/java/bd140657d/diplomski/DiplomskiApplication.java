792d89aed91d0685f57ee6fdb76f1ad81bdbb472
Djole <bozovic.djole@yahoo.com>
2019-06-24 16:11:29 +0200

    initial commit 2

---
+++ src/main/java/bd140657d/diplomski/DiplomskiApplication.java
@@ -0,0 +1,13 @@
+package bd140657d.diplomski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomskiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomskiApplication.class, args);
+	}
+
+}
